9fa82e9d14d2201890b8822f6997531d4a49aec2
Dario Aschero <dario.aschero@gmail.com>
2024-10-17 13:33:42 +0200

    opportunity card

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,14 +1,15 @@
 
 import Navbar from "../../components/navbar";
+import OpportunityCard from "../../components/opportunity-card";
 
 export default function Home() {
   return (
     <>
         <Navbar />
         <main className="flex-grow">
-          
-         
-          
+            <div className="flex flex-col gap-4 items-center mt-12 px-6">
+                <OpportunityCard />
+            </div>  
         </main>
         
     </>
