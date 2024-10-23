0e12f7bcd0a2e2eb06534f9f610f6bfc62a6a684
Dario Aschero <dario.aschero@gmail.com>
2024-10-23 10:34:12 +0200

    safter markdown solution

--- app/opportunity/[id]/page.tsx
+++ app/opportunity/[id]/page.tsx
@@ -2,7 +2,6 @@ import { notFound } from 'next/navigation';
 import { Opportunity } from '@/lib/types';
 import OpportunityDetail from '@/components/opportunity-detail';
 import Navbar from '@/components/navbar';
-import { markdownToHtml } from '@/lib/markdown';
 import { fetchApi } from "@/lib/api";
 
 async function getOpportunity(id: string): Promise<Opportunity | null> {
@@ -19,15 +18,12 @@ export default async function OpportunityPage({ params }: { params: { id: string
     notFound();
     return;
   }
-  
-  // Convert markdown content to HTML
-  const contentHtml = await markdownToHtml(opportunity.markdownContent);
 
   return ( 
     <>
       <Navbar />
       <main className="flex-grow">
-        <OpportunityDetail opportunity={opportunity} contentHtml={contentHtml} />
+        <OpportunityDetail opportunity={opportunity} />
       </main>
     </>
   );
--- components/opportunity-detail.tsx
+++ components/opportunity-detail.tsx
@@ -2,29 +2,28 @@ import { formatDistanceToNow } from 'date-fns';
 import { CircleUserRound } from "lucide-react"
 import { Opportunity } from '@/lib/types';
 import CredentialBadge from '@/components/credential-badge';
-
+import ReactMarkdown from 'react-markdown';
 
 interface OpportunityDetailProps {
   opportunity: Opportunity;
-  contentHtml: string;
 }
 
-export default function OpportunityDetail({ opportunity, contentHtml }: OpportunityDetailProps) {
+export default function OpportunityDetail({ opportunity }: OpportunityDetailProps) {
   const formattedDate = formatDistanceToNow(new Date(opportunity.postedDate), { addSuffix: true });
 
   return (
     <div className="max-w-2xl mx-auto p-6 mt-8 sm:mt-16 lg:mt-24">
-            <div className="flex items-center mb-4 text-sm text-muted-foreground">
+      <div className="flex items-center mb-4 text-sm text-muted-foreground">
         <CircleUserRound className="w-4 h-4 mr-2" />
         <span className="font-semibold mr-2">{opportunity.company}</span>
         <span>• Posted {formattedDate}</span>
       </div>
       <h1 className="text-4xl font-bold mb-4 tracking-tight">{opportunity.title}</h1>
 
-      <div 
-        className="mb-8 prose  prose-lg  prose-neutral dark:prose-invert"
-        dangerouslySetInnerHTML={{ __html: contentHtml }}
-      />
+      <div className="mb-8 prose prose-lg prose-neutral dark:prose-invert">
+        <ReactMarkdown>{opportunity.markdownContent}</ReactMarkdown>
+      </div>
+      
       <h2 className="text-xl font-semibold mb-4">Required Credentials</h2>
       <ul className="space-y-2">
         {opportunity.requiredCredentials.map((credential) => (
